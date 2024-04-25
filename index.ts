5fb37ba01b0a61b86898d4deef65f8160acaac50
Babra2023 <babra.riaz1@gmail.com>
2024-04-25 23:01:59 +0500

    Adding Word Counter Project

---
+++ index.ts
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+ // displaying colourfull message
+ console.log(chalk.bold.bgCyan("\n \t\t Its a typescript project - Word Counter"));
+ console.log("=" .repeat(50));
+
+ let answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter a sentence",
+    }
+ ]);
+
+ let words = answers.sentence.trim().split(" ");
+
+ console.log("=" .repeat(50));
+ console.log(chalk.bold("- Sentence Words:"));
+ console.log(words);
+ console.log(chalk.bold(`\n - Word Count:${chalk.bold.bgCyan(words.length)}`));
+ console.log("=". repeat(50));
